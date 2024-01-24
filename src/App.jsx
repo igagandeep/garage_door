b9147db9a2196520c58bd1b1a337e704a4c26e39
Gagandeep <gagan@prepr.org>
2024-01-24 16:53:35 -0500

    test 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import { HelmetProvider } from 'react-helmet-async';
 // import ShareThis from "./components/ShareThis"
 
 function App() {
-  const helmetContext = {};
   return (
-    <HelmetProvider context={helmetContext}>
+    <HelmetProvider>
       <h1>Hello React World</h1>
       <Share title="my title" description="this is description" url="https://superlative-bunny-c61262.netlify.app/" img="https://i.ibb.co/N2fppGk/logo3.png"/>
       
