fa7c47662924b1ca7a3fb8cb4b12cca358b93ce3
Gagandeep <gagan@prepr.org>
2024-01-24 10:50:08 -0500

    added react-helmet 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 
-import { HelmetProvider } from 'react-helmet-async';
 import Share from './components/Share';
-
 function App() {
-  
+
   return (
-    <HelmetProvider>
+    <>
       <h1>Hello React World</h1>
       <Share />
-    </HelmetProvider>
+    </>
   )
 }
 
--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,11 +1,11 @@
-// import {
-//     FacebookShareButton,
-//     TwitterShareButton,
-//     TwitterIcon,
-//     FacebookIcon,
-// } from "react-share";
-// import { Helmet } from 'react-helmet-async';
-import { ShareSocial } from 'react-share-social'
+import {
+    FacebookShareButton,
+    TwitterShareButton,
+    TwitterIcon,
+    FacebookIcon,
+} from "react-share";
+import { Helmet } from 'react-helmet';
+
 
 const Share = () => {
 
@@ -16,15 +16,37 @@ const Share = () => {
 
     return (
         <div>
+            <Helmet>
 
-            <ShareSocial
-                title={'I am happy to share my achievement'}
-                url="https://superlative-bunny-c61262.netlify.app"
-                socialTypes={['facebook', 'twitter', 'linkedin']}
-                onSocialButtonClicked={data => console.log(data)}
-            />
-            {/* <Helmet>
-                <title>Learning React Helmet!</title>
+                <title>Demo</title>
+                <meta charset="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="csrf_token" content="" />
+                <meta property="type" content="website" />
+                <meta property="url" content="https://superlative-bunny-c61262.netlify.app/" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                <meta name="theme-color" content="#ffffff" />
+                <meta name="_token" content="" />
+                <meta name="robots" content="noodp" />
+                <meta property="title" content="Demo" />
+                <meta property="quote" content="Demo1" />
+                <meta name="description" content="description" />
+                <meta property="image" content="https://superlative-bunny-c61262.netlify.app/preprLogo2.png" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="I am happy to share my achievement" />
+                <meta property="og:quote" content="I am happy to share my achievement" />
+                <meta property="og:hashtag" content="yo" />
+                <meta property="og:image" content="https://superlative-bunny-c61262.netlify.app/preprLogo2.png"/>
+                <meta content="image/*" property="og:image:type" />
+                <meta property="og:url" content="https://superlative-bunny-c61262.netlify.app/" />
+                <meta property="og:site_name" content="DSite name" />
+                <meta property="og:description" content="This is so cool" />
+
+{/* 
+
                 <meta name="description" content="We helps people to educate, learn" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://superlative-bunny-c61262.netlify.app/" />
@@ -38,14 +60,14 @@ const Share = () => {
                     content="https://superlative-bunny-c61262.netlify.app/" />
                 <meta property="twitter:title" content={item.name} />
                 <meta property="twitter:description" content="lorem" />
-                <meta property="twitter:image" content={item.img} />
+                <meta property="twitter:image" content={item.img} /> */}
             </Helmet>
             <FacebookShareButton url="https://superlative-bunny-c61262.netlify.app/">
                 <FacebookIcon sx={{ fontSize: 65 }} />
             </FacebookShareButton>
             <TwitterShareButton url="https://superlative-bunny-c61262.netlify.app/">
                 <TwitterIcon sx={{ fontSize: 65 }} />
-            </TwitterShareButton> */}
+            </TwitterShareButton>
 
 
         </div>
