d4f82e3205e97fbf966854d9ec0dfc7277bc1df1
Gagandeep <gagan@prepr.org>
2024-01-24 10:01:56 -0500

    added twitter share btn2

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,21 +1,29 @@
-import {
-    FacebookShareButton,
-    TwitterShareButton,
-    TwitterIcon,
-    FacebookIcon,
-} from "react-share";
-import { Helmet } from 'react-helmet-async';
+// import {
+//     FacebookShareButton,
+//     TwitterShareButton,
+//     TwitterIcon,
+//     FacebookIcon,
+// } from "react-share";
+// import { Helmet } from 'react-helmet-async';
+import { ShareSocial } from 'react-share-social'
 
 const Share = () => {
 
-    const item = {
-        name: "Demo",
-        img: "https://superlative-bunny-c61262.netlify.app/preprLogo2.png"
-    }
+    // const item = {
+    //     name: "Demo",
+    //     img: "https://superlative-bunny-c61262.netlify.app/preprLogo2.png"
+    // }
 
     return (
         <div>
-            <Helmet>
+
+            <ShareSocial
+                title={'I am happy to share my achievement'}
+                url="https://superlative-bunny-c61262.netlify.app"
+                socialTypes={['facebook', 'twitter', 'linkedin']}
+                onSocialButtonClicked={data => console.log(data)}
+            />
+            {/* <Helmet>
                 <title>Learning React Helmet!</title>
                 <meta name="description" content="We helps people to educate, learn" />
                 <meta property="og:type" content="website" />
@@ -25,23 +33,19 @@ const Share = () => {
                 <meta property="og:image" content={item.img} />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
-
-          
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url"
                     content="https://superlative-bunny-c61262.netlify.app/" />
                 <meta property="twitter:title" content={item.name} />
                 <meta property="twitter:description" content="lorem" />
                 <meta property="twitter:image" content={item.img} />
-
-
             </Helmet>
             <FacebookShareButton url="https://superlative-bunny-c61262.netlify.app/">
                 <FacebookIcon sx={{ fontSize: 65 }} />
             </FacebookShareButton>
             <TwitterShareButton url="https://superlative-bunny-c61262.netlify.app/">
                 <TwitterIcon sx={{ fontSize: 65 }} />
-            </TwitterShareButton>
+            </TwitterShareButton> */}
 
 
         </div>
