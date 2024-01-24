dd60268a0dad2fe74c2f536dec924572f9640822
Gagandeep <gagan@prepr.org>
2024-01-23 20:32:00 -0500

    fixed meta tag image issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-
-import { Helmet } from 'react-helmet';
-import { FacebookShareButton, FacebookIcon } from 'react-share';
+// import logo from "../public/preprLogo2.png";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+} from "react-share";
 
 function App() {
-  const item = {
-    title: "Prepr",
-    img: "https://superlative-bunny-c61262.netlify.app/preprLogo2.png"
-  };
-
+  
   return (
     <>
-      <Helmet>
-        <title>{item.title}</title>
-        <meta property="og:title" content={item.title} />
-        <meta property="og:image" content={item.img} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        {/* Other meta tags as needed */}
-      </Helmet>
-
       <h1>Hello World</h1>
       <FacebookShareButton url="https://superlative-bunny-c61262.netlify.app/">
-        <FacebookIcon size={65} />
+        <FacebookIcon sx={{ fontSize: 65 }} />
       </FacebookShareButton>
       {/* <img src={logo} /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
