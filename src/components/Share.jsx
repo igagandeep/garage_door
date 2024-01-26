60f43f758a58ba5fdb70ab675e482a8111b3eaee
Gagandeep <gagan@prepr.org>
2024-01-26 10:30:55 -0500

    added link 9

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,17 +1,16 @@
 
 const Share = () => {
 
-    let url = encodeURIComponent("https://cosmic-rabanadas-753078.netlify.app/share");
+    let url = encodeURIComponent("https://superlative-bunny-c61262.netlify.app/share");
 
     return (
         <div>
             <h2>Share Social</h2>
       
             <a target='_blank' className='c-black' rel="noreferrer" href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}>
-                Share on Linkedin
+                Share on Linkedin 
             </a>
-
-            
+  
         </div>
     );
 };
